64551bfca79567b5753cd0866aad666a982233cb
zlatandp <zlatandnepr@gmail.com>
2025-04-14 15:47:56 +0300

    Copied KidsTV b2b project at new repo

---
+++ app/PagesDispatcher.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import { useRouter, useSearchParams } from "next/navigation";
+import { nextStep } from "@/app/actions/steps-client.action";
+import { StepType } from "@/app/actions/actions.types";
+import { getCurrentTime } from "@/app/actions/steps.action";
+import { useState, useEffect } from "react";
+
+export default function PagesDispatcher() {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const [pageEnter, setPageEnter] = useState<boolean>(false);
+
+  const utmContent = searchParams.get("utm_content") || "";
+  const banner = searchParams.get("banner") || "";
+  const campaign = searchParams.get("campaign") || "";
+
+  useEffect(() => {
+    const goToNextStep = async () => {
+      await nextStep({
+        step: 0,
+        type: StepType.Proxy,
+        question: "Разведение по флоу",
+        answer: {
+          utm_content: utmContent,
+          banner: banner,
+          campaign: campaign,
+        },
+        time: await getCurrentTime(),
+      });
+    };
+    goToNextStep()
+      .catch((error) => console.log(error))
+      .finally(() => setPageEnter(true));
+  }, []);
+
+  useEffect(() => {
+    switch (utmContent) {
+      default:
+        router.push(`/kidstvb2b`);
+    }
+  }, [pageEnter]);
+
+  return (
+    <>
+      <div className="loader">
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </>
+  );
+}
